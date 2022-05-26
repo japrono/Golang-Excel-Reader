1fb34eb0f6d37c8258ec4dee12019875ad5e79d8
Jakub Pronobis <jakub.pronobis@gmail.com>
2022-05-26 14:44:23 +0100

    extend email domain parsing function to include username

--- interview.go
+++ interview.go
@@ -8,9 +8,19 @@ import (
   "strconv"
   "fmt"
   "strings"
+  "encoding/csv"
+  "io"
+  "log"
+  "os"
 )
 
-func GetEmailDomain(EmailAddress string) (bool, string) {
+type EmailDomain struct {
+    Domain string
+    Count     int
+    Emails    []int
+}
+
+func GetEmailDomain(EmailAddress string) (bool, string, string) {
   at := strings.LastIndex(EmailAddress, "@")
   if at >= 0 {
       username, domain := EmailAddress[:at], EmailAddress[at+1:]
@@ -20,19 +30,59 @@ func GetEmailDomain(EmailAddress string) (bool, string) {
       if username == "" || domain == "" {
         is_valid_email = false
         domain = ""
+        username = ""
       }
 
-      return is_valid_email, domain
+      return is_valid_email, domain, username
   } else {
       var is_valid_email = false
       var domain = ""
-      return is_valid_email, domain
+      var username = ""
+      return is_valid_email, domain, username
   }
 }
 
+func ParseExcelFile() (bool, string) {
+  f, err := os.Open("customers.csv")
+
+  if err != nil {
+      log.Fatal(err)
+      return false, ""
+  }
+
+  defer f.Close()
+
+  csvReader := csv.NewReader(f)
+  for {
+      rec, err := csvReader.Read()
+      if err == io.EOF {
+          break
+      }
+      if err != nil {
+          log.Fatal(err)
+      }
+
+      fmt.Printf("%+v\n", rec)
+      fmt.Printf("%v %v %v %v", rec[0], rec[1], rec[2], rec[3])
+      var is_valid_email, EmailDomain, username = GetEmailDomain(rec[2])
+      fmt.Printf("\n")
+      fmt.Printf("%v %v %v", is_valid_email, EmailDomain, username)
+
+      fmt.Printf("\n")
+
+  }
+
+
+  return true, ""
+}
+
 func main() {
-    var is_valid_email, Domain = GetEmailDomain("jakub.pronobis@gmail.com")
+    var is_valid_email, Domain, username = GetEmailDomain("jakub.pronobis@gmail.com")
     s := strconv.FormatBool(is_valid_email)
     fmt.Println("value of is_valid_email = " + s)
     fmt.Println("value of Domain = " + Domain)
+    fmt.Println("value of username = " + username)
+
+    ParseExcelFile();
+
 }
--- interview_test.go
+++ interview_test.go
@@ -10,22 +10,25 @@ func TestGetEmailDomain(t *testing.T) {
     EmailAddress string
     want1 bool
     want2 string
+    want3 string
 }{
-    {"jakub.pronobis@gmail.com", true, "gmail.com"},
-    {"jakub.pronobis@@gmail.com", true, "gmail.com"},
-    {"jakub.pronobis@@outlook.com", true, "outlook.com"},
-    {"jakub.pronobis", false, ""},
-    {"", false, ""},
-    {"@g.com", false, ""},
-    {"user@", false, ""},
+    {"jakub.pronobis@gmail.com", true, "gmail.com", "jakub.pronobis"},
+    {"jakub.pronobis@@gmail.com", true, "gmail.com", "jakub.pronobis@"},
+    {"jakub.pronobis@@outlook.com", true, "outlook.com", "jakub.pronobis@"},
+    {"jakub.pronobis", false, "", ""},
+    {"", false, "", ""},
+    {"@g.com", false, "", ""},
+    {"user@", false, "", ""},
+    {"@", false, "", ""},
+    {"jakub@@@gmail.com", true, "gmail.com", "jakub@@"},
 }
 
   for _, tt := range tests {
-      testname := fmt.Sprintf("%v,%v,%v", tt.EmailAddress, tt.want1, tt.want2)
+      testname := fmt.Sprintf("%v,%v,%v,%v", tt.EmailAddress, tt.want1, tt.want2, tt.want3)
       t.Run(testname, func(t *testing.T) {
-          ans1, ans2 := GetEmailDomain(tt.EmailAddress)
-          if ans1 != tt.want1 || ans2 != tt.want2 {
-              t.Errorf("got %v %v, want %v %v", ans1, ans2, tt.want1, tt.want2)
+          ans1, ans2, ans3 := GetEmailDomain(tt.EmailAddress)
+          if ans1 != tt.want1 || ans2 != tt.want2 || ans3 != tt.want3 {
+              t.Errorf("got %v %v %v, want %v %v %v", ans1, ans2, ans3, tt.want1, tt.want2, tt.want3)
           }
       })
   }
