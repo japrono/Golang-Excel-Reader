148905451b29aa122e9b0eab8ac64732fa0bde68
Jakub Pronobis <jakub.pronobis@gmail.com>
2022-05-26 16:22:53 +0100

    dev

--- interview.go
+++ interview.go
@@ -3,9 +3,9 @@
 // of customers with e-mail addresses for each domain.  Any errors should be
 // logged (or handled). Performance matters (this is only ~3k lines, but *could*
 // be 1m lines or run on a small machine).
+
 package main
 import (
-  "strconv"
   "fmt"
   "strings"
   "encoding/csv"
@@ -14,10 +14,10 @@ import (
   "os"
 )
 
-type EmailDomain struct {
-    Domain string
+type Domain struct {
+    Domain    string
     Count     int
-    Emails    []int
+    Emails    []string
 }
 
 func GetEmailDomain(EmailAddress string) (bool, string, string) {
@@ -42,12 +42,27 @@ func GetEmailDomain(EmailAddress string) (bool, string, string) {
   }
 }
 
-func ParseExcelFile() (bool, string) {
-  f, err := os.Open("customers.csv")
+func AreHeadersValid(Header1 string, Header2 string, Header3 string, Header4 string) (bool) {
+  if(Header1 == "first_name" && Header2 == "last_name" && Header3 == "email" && Header4 == "gender") {
+    return true
+  } else {
+    return false
+  }
+}
+
+func IncreaseCounter(EmailDomains []Domain, EmailDomainToIncrease string, Email string) (bool, []Domain) {
+  return true, []Domain{}
+}
+
+func ParseExcelFile() (bool, []Domain) {
+  EmailDomains := []Domain{}
+  HeaderValidated := false
+
+  f, err := os.Open("customers_test_1.csv")
 
   if err != nil {
       log.Fatal(err)
-      return false, ""
+      return false, []Domain{}
   }
 
   defer f.Close()
@@ -55,34 +70,70 @@ func ParseExcelFile() (bool, string) {
   csvReader := csv.NewReader(f)
   for {
       rec, err := csvReader.Read()
+
       if err == io.EOF {
           break
       }
       if err != nil {
           log.Fatal(err)
+          return false, []Domain{}
       }
 
-      fmt.Printf("%+v\n", rec)
-      fmt.Printf("%v %v %v %v", rec[0], rec[1], rec[2], rec[3])
+      if HeaderValidated == false {
+        if AreHeadersValid(rec[0], rec[1], rec[2], rec[3]) {
+          HeaderValidated = true
+          continue
+        } else {
+          log.Fatal("Excel headers are not correct")
+          return false, []Domain{}
+        }
+      }
+
       var is_valid_email, EmailDomain, username = GetEmailDomain(rec[2])
-      fmt.Printf("\n")
-      fmt.Printf("%v %v %v", is_valid_email, EmailDomain, username)
 
-      fmt.Printf("\n")
+      if is_valid_email == true {
+        var IncreasedCounter = false
 
+        for index := range EmailDomains {
+          if EmailDomains[index].Domain == EmailDomain {
+            EmailDomains[index].Count = EmailDomains[index].Count + 1
+            EmailDomains[index].Emails = append(EmailDomains[index].Emails, username)
+            IncreasedCounter = true
+          }
+        }
+
+        if IncreasedCounter == false {
+          EmailDomainObj := Domain{Domain: EmailDomain, Count: 1, Emails: []string{username} }
+          EmailDomains = append(EmailDomains, EmailDomainObj)
+        }
+      } else {
+        fmt.Printf("Warning: This email address is not valid: %v", rec[2])
+        fmt.Printf("\n")
+      }
   }
 
+  return true, EmailDomains
+}
 
-  return true, ""
+func PrintResults(EmailsDomains []Domain) {
+  for index := range EmailsDomains {
+      fmt.Printf("%v %v", EmailsDomains[index].Count, EmailsDomains[index].Domain)
+      fmt.Printf("\n")
+
+      for index2 := range EmailsDomains[index].Emails {
+        fmt.Printf("\t %v", EmailsDomains[index].Emails[index2])
+      }
+
+      fmt.Printf("\n\n")
+  }
 }
 
 func main() {
-    var is_valid_email, Domain, username = GetEmailDomain("jakub.pronobis@gmail.com")
-    s := strconv.FormatBool(is_valid_email)
-    fmt.Println("value of is_valid_email = " + s)
-    fmt.Println("value of Domain = " + Domain)
-    fmt.Println("value of username = " + username)
+    ParsedSuccess, EmailsDomains := ParseExcelFile();
 
-    ParseExcelFile();
-
+    if ParsedSuccess == true {
+      PrintResults(EmailsDomains)
+    } else {
+      fmt.Printf("excel file parse failed")
+    }
 }
